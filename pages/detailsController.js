5d8d68349f6ba490b544f2125673b4bd70066434
andreabarbata <andrea.barbata.ab@gmail.com>
2016-04-14 14:33:29 +0200

    added login layout rubato a pette

--- pages/detailsController.js
+++
@@ -1,8 +0,0 @@
-angular.module('routerApp')
-    .controller('detailsController', function($scope, $state, $stateParams){
-        $scope.id = $stateParams.id;
-        var currentObj = data().getById($stateParams.id);
-
-        $scope.name = currentObj.name;
-
-    });
