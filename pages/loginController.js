0bfa9dbdc05a41602b263b19c9bc4c48158aa4f9
LucaCampana <tsac028@tecnicosuperiorekennedy.it>
2016-04-14 16:35:07 +0200

    Added login functionality

--- pages/loginController.js
+++ pages/loginController.js
@@ -1,20 +1,29 @@
 angular.module('routerApp')
     .controller('loginController', function(LoginFactory, $scope, $state){
 
-        $scope.email = "tsac-2015@tecnicosuperiorekennedy.it";
-        $scope.password = "tsac"; //<-----TODO da prendere entrambi nella pagina con angular
+        //$scope.email = "tsac-2015@tecnicosuperiorekennedy.it";
+        //$scope.password = "tsac";
+
+        $scope.email = "";
+        $scope.password = "";
 
         $scope.login = function(){
+            alert("Login con " + $scope.email + " " + $scope.password);
             LoginFactory.login($scope.email, $scope.password,
                 function(err, result) {
-                    if (err) return console.log("Errore cosa? ", err);
+                    if (err) {
+                        error();
+                        return console.log("Errore cosa? ", err);
+                    }
                     console.log("Uo", result);
                     console.log(result.session);
                     $state.go('home', {session: result.session});
                 });
         };
 
-        //TODO controllo correttezza email password, gestione errori
-
+        function error(){
+            //TODO scrivere modali ultratamarri o non so cosa
+            alert("Email o password errati");
+        }
         //$scope.login();
     });
