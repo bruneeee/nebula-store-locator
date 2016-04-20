c41f6a3637e2746643f192f9af2119585ac71d59
bruneeee <nikibrunello96@gmail.com>
2016-04-20 17:37:36 +0200

    added distance and order

--- app/controllers/storesController.js
+++ app/controllers/storesController.js
@@ -1,11 +1,54 @@
-storeLocator.controller("storesController", function($rootScope, $scope, storesService) {
+storeLocator.controller("storesController", function($rootScope, $scope, storesService,localizationManager) {
+
+    var myLat ;
+    var myLng ;
+    $scope.distances = [];
+
+    localizationManager.getPosition(function(data){
+        if(data){
+            myLat = data.coords.latitude;
+            myLng = data.coords.longitude;
+        }
+        else{
+            myLat = 0;
+            myLng = 0;
+            console.log("No localizzazione, gg");
+        }
+
+    });
+
     storesService.getAll(
         function(data){
             $scope.stores = data;
+            gg();
         }
 );
+    function gg(){
+        for(var i=0;i < $scope.stores.length;i++){
+
+            storesService.distance(myLat,myLng,$scope.stores[i].latitude,$scope.stores[i].longitude,function(res){
+                $scope.stores[i].distance = res;
+                //console.log(res);
+            });
+        }
+    }
+
     $scope.focus = function (lat,lng){
         $rootScope.$broadcast('focusOn',[lat,lng]);
     }
 
 });
+storeLocator.filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+        var filtered = [];
+        angular.forEach(items, function(item) {
+            filtered.push(item);
+        });
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+        if(reverse) filtered.reverse();
+        return filtered;
+    };
+});
+
--- app/services/storesService.js
+++ app/services/storesService.js
@@ -39,4 +39,18 @@ storeLocator.service('storesService', function ($http, sessionManager, RequestUR
                 })
         }
     };
+
+    this.distance = function getDistance(lat1,lon1,lat2,lon2, completionHandler) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = (lat2-lat1) * (Math.PI/180);
+        var dLon = (lon2-lon1) * (Math.PI/180);
+        var a =
+                Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(lat1 * (Math.PI/180)) * Math.cos(lat2 * (Math.PI/180)) *
+                Math.sin(dLon/2) * Math.sin(dLon/2)
+            ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        return completionHandler(d);
+    };
 });
