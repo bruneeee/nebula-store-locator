85fa86a6e92debd4c7c195d39790d0b59b71d31e
bruneeee <nikibrunello96@gmail.com>
2016-04-20 16:51:27 +0200

    click now move the map

--- app/controllers/storesController.js
+++ app/controllers/storesController.js
@@ -1,7 +1,11 @@
-storeLocator.controller("storesController", function($scope, storesService) {
+storeLocator.controller("storesController", function($rootScope, $scope, storesService) {
     storesService.getAll(
         function(data){
             $scope.stores = data;
         }
 );
+    $scope.focus = function (lat,lng){
+        $rootScope.$broadcast('focusOn',[lat,lng]);
+    }
+
 });
